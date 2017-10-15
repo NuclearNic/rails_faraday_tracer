e88cdc5e760d21067103370bf48a62cde5caae8f
NuclearNic <nuclearnicdev@gmaill.com>
2017-10-15 18:39:51 +0200

    Add initializer requirement to main gem module

--- lib/rails_faraday_tracer.rb
+++ lib/rails_faraday_tracer.rb
@@ -1,5 +1,6 @@
-require "rails_faraday_tracer/version"
-require "rails_faraday_tracer/railtie"
+require 'rails_faraday_tracer/version'
+require 'rails_faraday_tracer/railtie'
+require 'generators/opentracing_initializer'
 # require "rails_faraday_tracer/traced_request.rb"
 # require "rails_faraday_tracer/tracer_middleware"
 # require "rails_faraday_tracer/faraday_tracer_middleware"
