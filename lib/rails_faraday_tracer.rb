a8ceb956d6ff8f748e1fcf19818d84c1becdb348
NuclearNic <nuclearnicdev@gmaill.com>
2017-10-15 17:57:05 +0200

    Add OpenTracing initializer

--- lib/rails_faraday_tracer.rb
+++ lib/rails_faraday_tracer.rb
@@ -1,5 +1,10 @@
 require "rails_faraday_tracer/version"
+require "rails_faraday_tracer/railtie"
+# require "rails_faraday_tracer/traced_request.rb"
+# require "rails_faraday_tracer/tracer_middleware"
+# require "rails_faraday_tracer/faraday_tracer_middleware"
+# require "rails_faraday_tracer/opentracing_initializer"
+
 
 module RailsFaradayTracer
-  # Your code goes here...
 end
